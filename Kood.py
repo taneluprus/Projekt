5d52b2ac025d70e52e0da789c6ba73a956ede435
Tanel Üprus <tanel.uprus@real.edu.ee>
2024-04-19 15:11:48 +0300

    Various html, flask etc

--- Kood.py
+++ Kood.py
@@ -4,6 +4,25 @@ import random
 from flask import Flask, render_template, request, redirect, url_for
 
 app=Flask(__name__, template_folder="templates")
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 folder=('Music1\\')
 list=[]
