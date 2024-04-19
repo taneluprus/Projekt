69063610c31b3f3dffd10581f36b560b0f1b15a2
Tanel Üprus <tanel.uprus@real.edu.ee>
2024-04-19 14:46:43 +0300

    Music player code

--- Kood.py
+++ Kood.py
@@ -1,3 +1,22 @@
 import winsound
+import os
+import random
 
-winsound.PlaySound("testmusicfolder/ARTIST - test.wav", 0)
+folder=('Music1\\')
+list=[]
+artistid=[]
+laulud=[]
+number=int(0)
+
+list=os.listdir('testmusicfolder\\'+folder)
+print(list)
+
+for element in list:
+    artist, laul = list[number].split(" - ")
+    number=number+1
+    artistid.append(artist)
+    laulud.append(laul)
+
+print(random.choice(list))
+
+winsound.PlaySound((("testmusicfolder\\"+folder)+(random.choice(list))), winsound.SND_FILENAME)
