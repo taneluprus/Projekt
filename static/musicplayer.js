24a980b864cabe2066a011b168efa215a08c69f5
aloaloolaola <alo.virunurm@gmail.com>
2024-05-10 15:01:32 +0300

    artism

--- static/musicplayer.js
+++ static/musicplayer.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-    files = ['ARTIST - test.wav', 'ARTIST2 - test2.wav', 'ARTIST3 - test3.wav', 'ARTIST4 - test.wav4', 'ARTIST5 - test5.wav'],
-    i = Math.floor(Math.random()*files.length);
-    var array = files[i].split(' - '),
-    artist = array[0], track = array[1];
-    alert(artist, track)
-
+  
 }
+files = ['ARTIST - test.wav', 'ARTIST2 - test2.wav', 'ARTIST3 - test3.wav', 'ARTIST4 - test.wav4', 'ARTIST5 - test5.wav'],
+i = Math.floor(Math.random()*files.length);
+var array = files[i].split(' - '),
+artist = array[0], track = array[1];
+alert(artist, track)
 
 
 
@@ -16,5 +16,5 @@ function play() {
   }
 
 function check() {
-    
+    alert(artist)
 }
