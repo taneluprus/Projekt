957fef80f84f0b461b6c29272a71900d0b651032
Tanel Üprus <tanel.uprus@real.edu.ee>
2024-05-10 14:29:09 +0300

    Musicplayer + file movement + etc

---
+++ static/musicplayer.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    files = ['ARTIST - test.wav', 'ARTIST2 - test2.wav', 'ARTIST3 - test3.wav', 'ARTIST4 - test.wav4', 'ARTIST5 - test5.wav'],
+    i = Math.floor(Math.random()*files.length);
+    var array = files[i].split(' - '),
+    artist = array[0], track = array[1];
+
+}
+
+
+function play() {
+    var path = 'static/testmusicfolder/Music1/'
+    var audio = new Audio(path+files[i]);
+    audio.play();
+  }
