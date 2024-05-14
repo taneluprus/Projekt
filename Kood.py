b4f414696792ed100850b33ae9083fabd694bf25
aloaloolaola <alo.virunurm@gmail.com>
2024-05-14 12:53:24 +0300

    removed old player

--- Kood.py
+++ Kood.py
@@ -1,6 +1,3 @@
-import winsound
-import os
-import random
 from flask import Flask, render_template, request, redirect, url_for
 
 app=Flask(__name__, template_folder="templates")
@@ -26,24 +23,3 @@ def favicon():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-folder=('Music1\\')
-list=[]
-artistid=[]
-laulud=[]
-number=int(0)
-
-list=os.listdir('testmusicfolder\\'+folder)
-print(list)
-
-for element in list:
-    artist, laul = list[number].split(" - ")
-    number=number+1
-    artistid.append(artist)
-    laulud.append(laul)
-
-print(random.choice(list))
-
-winsound.PlaySound((("testmusicfolder\\"+folder)+(random.choice(list))), winsound.SND_FILENAME)
